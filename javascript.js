81555f89779f0c388f7c7cf663c35f6f2dbd1d44
inessev <123724101+inessev@users.noreply.github.com>
2023-04-14 19:13:15 +0200

    corrección final

--- javascript.js
+++ javascript.js
@@ -3,15 +3,15 @@ var number1= prompt("Escriba aquí el primer numero");
 var number2= prompt("Escriba aquí el segundo numero");
 
 if(number1>number2){
-    alert("El "+ number1+ " es mayor que "+ number2);
+    alert("El "+ number1+ " es mayor que el "+ number2);
 }else{
-    alert("El "+ number1+ " no es mayor que "+ number2);
+    alert("El "+ number1+ " no es mayor que el "+ number2);
 }
 
 if(number1<=number2){
-    alert("El "+ number1+" es menor o igual que "+ number2);
+    alert("El "+ number1+" es menor o igual que el "+ number2);
 }else{
-    alert("El "+ number1+" no es menor o igual que "+ number2);
+    alert("El "+ number1+" no es menor o igual que el "+ number2);
 }
 
 if(number2%2==0){
