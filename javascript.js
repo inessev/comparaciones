693f39c3b465b7bf7ebc83bd06e27c55cc903638
inessev <123724101+inessev@users.noreply.github.com>
2023-04-14 18:27:23 +0200

    prueba

--- javascript.js
+++ javascript.js
@@ -3,7 +3,6 @@ var number1= prompt("Escriba aquí el primer numero");
 var number2= prompt("Escriba aquí el segundo numero");
 let resultado = number1 > number2; 
 alert("El " + number1+ " es mayor que el "+ number2+" = "+ resultado );
-let resultado = number1 > number2; 
-alert("El " + number1+ " es menor que el "+ number2+" = "+ resultado );
-let resultado = number1 == number2; 
-alert("El " + number1+ " es igual que el "+ number2+" = "+ resultado );
+let resultado = number1 >= number2; 
+alert("El " + number1+ " es menor o igual que el "+ number2+" = "+ resultado );
+
