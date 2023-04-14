dd65d0462c1b36896d1d4bdd1a58745629dce99b
inessev <123724101+inessev@users.noreply.github.com>
2023-04-14 18:43:06 +0200

    prueba final

--- javascript.js
+++ javascript.js
@@ -5,4 +5,13 @@ let resultado = number1 > number2;
 alert("El " + number1+ " es mayor que el "+ number2+" = "+ resultado );
 let resultado2 = number1 <= number2; 
 alert("El " + number1+ " es menor o igual que el "+ number2+" = "+ resultado2 );
-
+if(number1%2==0){
+    alert("El número "+ number1+" es par");
+}else{
+    alert("El número "+ number1+" es impar");
+}
+if(number2%2==0){
+    alert("El número "+ number2+" es par");
+}else{
+    alert("El número "+ number2+" es impar");
+}
