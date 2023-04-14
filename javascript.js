a35fff7a9375b46bac41ce76f45199ffd2caf776
inessev <123724101+inessev@users.noreply.github.com>
2023-04-14 18:16:44 +0200

    prueba1

---
+++ javascript.js
@@ -0,0 +1,5 @@
+alert ("Escriba dos números para hacer una serie de comparaciones");
+var number1= prompt("Escriba aquí el primer numero");
+var number2= prompt("Escriba aquí el segundo numero");
+let result = number1 > number2; 
+alert( result );
