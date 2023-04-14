49b553a10cf272e26ed8c21680e6b871e129ee12
inessev <123724101+inessev@users.noreply.github.com>
2023-04-14 19:01:48 +0200

    corrección final

--- javascript.js
+++ javascript.js
@@ -1,12 +1,22 @@
 alert ("Escriba dos números para hacer una serie de comparaciones");
 var number1= prompt("Escriba aquí el primer numero");
 var number2= prompt("Escriba aquí el segundo numero");
-let resultado = number1 > number2; 
-alert("El " + number1+ " es mayor que el "+ number2+" = "+ resultado );
-let resultado2 = number1 <= number2; 
-alert("El " + number1+ " es menor o igual que el "+ number2+" = "+ resultado2 );
+
+if(number1>number2){
+    alert("El mayor es "+ number1);
+}else{
+    alert("El mayor es "+ number2);
+}
+
+if(number1<=number2){
+    alert("El número "+ number1+" es menor o igual que "+ number2);
+}else{
+    alert("El número "+ number1+" NO es menor o igual que "+ number2);
+}
+
 if(number2%2==0){
     alert("El número "+ number2+" es par");
 }else{
     alert("El número "+ number2+" es impar");
 }
+
