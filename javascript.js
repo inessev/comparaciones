02f9dc9601633553fc375e69ec6a3a8cc7886ba3
inessev <123724101+inessev@users.noreply.github.com>
2023-04-14 18:23:46 +0200

    prueba español

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 alert ("Escriba dos números para hacer una serie de comparaciones");
 var number1= prompt("Escriba aquí el primer numero");
 var number2= prompt("Escriba aquí el segundo numero");
-let result = number1 > number2; 
-alert("El " + number1+ " es mayor que el "+ number2+" = "+ result );
+let resultado = number1 > number2; 
+alert("El " + number1+ " es mayor que el "+ number2+" = "+ resultado );
+
