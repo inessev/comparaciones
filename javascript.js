51a544193b1a2f2ed564c8795a7ce54c286bcab1
inessev <123724101+inessev@users.noreply.github.com>
2023-04-18 12:09:29 +0200

    prueba 2

--- javascript.js
+++ javascript.js
@@ -6,11 +6,10 @@ if(number1>number2){
     alert("El "+ number1+ " es mayor que el "+ number2);
 }else if(number1<number2){
     alert("El "+ number1+ " es menor que el "+ number2);
-}
-
-if(number1==number2){
+}else if(number1==number2){
     alert("El "+ number1+ " es igual que el "+ number2);
 }
+
 if(number2%2==0){
     alert("El "+ number2+" es par");
 }else{
